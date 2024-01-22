9a5df8d0d1e73adfc5cb2a5ee322e8966e914fb4
andrijajovanovic98 <andrija.jovanovic1998@gmail.com>
2024-01-22 10:20:34 +0100

    Initial commit

---
+++ src/users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Profile, Location
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Location,LocationAdmin)
+
+
+
+
+# Register your models here.
